5584a3d49ead6b85dfe1f58634b020e9de440816
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 12:46:40 +0300

    added categories

--- Apps.Plunet/PlunetApplication.cs
+++ Apps.Plunet/PlunetApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Plunet;
 
-public class PlunetApplication : IApplication
+public class PlunetApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.TranslationBusinessManagement];
+        set { }
+    }
+    
     public string Name
     {
         get => "Plunet";
