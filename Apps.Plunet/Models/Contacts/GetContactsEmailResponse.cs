f8d471445e13568c8baf9117b216689e8077fa45
Cecilia Nistal <cecilia.nistal@centific.com>
2024-04-19 11:22:17 -0300

    Added Get emails action

---
+++ Apps.Plunet/Models/Contacts/GetContactsEmailResponse.cs
@@ -0,0 +1,15 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Plunet.Models.Contacts
+{
+    public class GetContactsEmailResponse
+    {
+        [Display("Email Addresses")]
+        public IEnumerable<string> EmailAddresses { get; set; }
+    }
+}
