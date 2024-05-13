869737e98d9bf09298c67ff784366a97621363a4
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-05-13 17:44:02 +0300

    Maybe fixed to stream method

--- Apps.Plunet/Models/Invoices/Common/InvoicesObject.cs
+++ Apps.Plunet/Models/Invoices/Common/InvoicesObject.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text;
+using Newtonsoft.Json;
 
 namespace Apps.Plunet.Models.Invoices.Common;
 
@@ -9,14 +10,15 @@ public class InvoicesObject
     public Stream ToStream()
     {
         var stream = new MemoryStream();
-        
+    
         var json = JsonConvert.SerializeObject(this, Formatting.Indented);
-        using var writer = new StreamWriter(stream);
-        
-        writer.Write(json);
-        writer.Flush();
+        using (var writer = new StreamWriter(stream, encoding: Encoding.UTF8, bufferSize: 1024, leaveOpen: true))
+        {
+            writer.Write(json);
+            writer.Flush();
+        }
+    
         stream.Position = 0;
-
         return stream;
     }
 }
