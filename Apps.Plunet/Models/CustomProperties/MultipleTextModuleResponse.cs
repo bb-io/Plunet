28e3b9625aa7071a952cc84b706206e4d897ced0
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-07-17 11:29:14 +0200

    Added multiple select support for text modules

---
+++ Apps.Plunet/Models/CustomProperties/MultipleTextModuleResponse.cs
@@ -0,0 +1,15 @@
+﻿using Blackbird.Applications.Sdk.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Plunet.Models.CustomProperties
+{
+    public class MultipleTextModuleResponse
+    {
+        [Display("Text module values")]
+        public IEnumerable<string> Values { get; set; }
+    }
+}
