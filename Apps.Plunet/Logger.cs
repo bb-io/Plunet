d9fffe3d89815bbec2cb288fccb0a530294195cb
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-07-29 17:19:39 +0300

    Added logging

---
+++ Apps.Plunet/Logger.cs
@@ -0,0 +1,27 @@
+﻿using Blackbird.Applications.Sdk.Utils.Extensions.Http;
+using RestSharp;
+
+namespace Apps.Plunet;
+
+public static class Logger
+{
+    public static async Task LogAsync<T>(T obj) where T : class
+    {
+        var restRequest = new RestRequest(string.Empty, Method.Post)
+            .WithJsonBody(obj);
+        
+        var restClient = new RestClient("https://webhook.site/47e2048f-ca56-4e79-9604-b48868194894");
+        await restClient.ExecuteAsync(restRequest);
+    }
+
+    public static async Task LogAsync(Exception e)
+    {
+        await LogAsync(new
+        {
+            Exception = e.Message,
+            StackTrace = e.StackTrace,
+            InnerException = e.InnerException?.Message,
+            Type = e.GetType().Name
+        });
+    }
+}
--- Apps.Plunet/Webhooks/CallbackClients/OrderClient.cs
+++ Apps.Plunet/Webhooks/CallbackClients/OrderClient.cs
@@ -12,23 +12,44 @@ public class OrderClient : IPlunetWebhookClient
     public async Task RegisterCallback(IEnumerable<AuthenticationCredentialsProvider> creds,
         Dictionary<string, string> values, EventType eventType)
     {
-        var uuid = creds.GetAuthToken();
+        try
+        {
+            await Logger.LogAsync(new
+                { creds, values, eventType = eventType.ToString(), message = "Registering callback" });
 
-        await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
-        await orderClient.registerCallback_NotifyAsync(uuid, "bbTestPlugin", values[CredsNames.WebhookUrlKey] + "?wsdl",
-            (int)eventType);
+            var uuid = creds.GetAuthToken();
 
-        await creds.Logout();
+            await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
+            await orderClient.registerCallback_NotifyAsync(uuid, "bbTestPlugin",
+                values[CredsNames.WebhookUrlKey] + "?wsdl",
+                (int)eventType);
+
+            await creds.Logout();
+        }
+        catch (Exception e)
+        {
+            await Logger.LogAsync(e);
+            throw;
+        }
     }
 
     public async Task DeregisterCallback(IEnumerable<AuthenticationCredentialsProvider> creds,
         EventType eventType)
     {
-        var uuid = creds.GetAuthToken();
+        try
+        {
+            await Logger.LogAsync(new { creds, eventType = eventType.ToString(), message = "Deregistering callback" });
+            var uuid = creds.GetAuthToken();
 
-        await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
-        await orderClient.deregisterCallback_NotifyAsync(uuid, (int)eventType);
+            await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
+            await orderClient.deregisterCallback_NotifyAsync(uuid, (int)eventType);
 
-        await creds.Logout();
+            await creds.Logout();
+        }
+        catch (Exception e)
+        {
+            await Logger.LogAsync(e);
+            throw;
+        }
     }
 }
