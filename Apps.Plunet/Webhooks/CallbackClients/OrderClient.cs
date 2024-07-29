5e3b74247ac524ef84633beb26d4e38d201e7721
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-07-29 18:43:44 +0300

    added smart unsubscribing logic

--- Apps.Plunet/Webhooks/CallbackClients/OrderClient.cs
+++ Apps.Plunet/Webhooks/CallbackClients/OrderClient.cs
@@ -4,6 +4,7 @@ using Apps.Plunet.Extensions;
 using Apps.Plunet.Webhooks.CallbackClients.Base;
 using Apps.Plunet.Webhooks.Models;
 using Blackbird.Applications.Sdk.Common.Authentication;
+using Microsoft.Extensions.Logging;
 
 namespace Apps.Plunet.Webhooks.CallbackClients;
 
@@ -47,10 +48,24 @@ public class OrderClient : IPlunetWebhookClient
             await Logger.LogAsync(new { values, eventType = eventType.ToString(), message = "Unsubscribing callback" });
             var uuid = creds.GetAuthToken();
 
+            var dataAdminClient = Clients.GetAdminClient(creds.GetInstanceUrl());
+
+            var callbacks = await dataAdminClient.getListOfRegisteredCallbacksAsync(uuid);
+            var eventCallbacks = callbacks.data.Where(c => c.eventType == (int)eventType).ToList();
+            
+            await Logger.LogAsync(new { eventCallbacks });
+
             await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
             var result = await orderClient.deregisterCallback_NotifyAsync(uuid, (int)eventType);
 
             await Logger.LogAsync(new { result_message = result.statusMessage, status_code = result.statusCode });
+            
+            foreach(var callback in eventCallbacks.Where(x => x.serverAddress != values[CredsNames.WebhookUrlKey] + "?wsdl"))
+            {
+                await orderClient.registerCallback_NotifyAsync(uuid, "bbTestPlugin",
+                    callback.serverAddress, (int)eventType);
+            }
+            
             await creds.Logout();
         }
         catch (Exception e)
