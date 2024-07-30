29f6122159ce18324bcfe9f48f396607ae8308be
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-07-30 10:35:28 +0300

    Fake commit

--- Apps.Plunet/Webhooks/CallbackClients/OrderClient.cs
+++ Apps.Plunet/Webhooks/CallbackClients/OrderClient.cs
@@ -13,67 +13,51 @@ public class OrderClient : IPlunetWebhookClient
     public async Task RegisterCallback(IEnumerable<AuthenticationCredentialsProvider> creds,
         Dictionary<string, string> values, EventType eventType)
     {
-        try
-        {
-            await Logger.LogAsync(new
-                { 
-                    values, 
-                    eventType = eventType.ToString(), 
-                    message = "Subscribing callback" 
-                });
+        await Logger.LogAsync(new
+        { 
+            values, 
+            eventType = eventType.ToString(), 
+            message = "Subscribing callback" 
+        });
 
-            var uuid = creds.GetAuthToken();
+        var uuid = creds.GetAuthToken();
 
-            await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
-            var result = await orderClient.registerCallback_NotifyAsync(uuid, "bbTestPlugin",
-                values[CredsNames.WebhookUrlKey] + "?wsdl",
-                (int)eventType);
+        await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
+        var result = await orderClient.registerCallback_NotifyAsync(uuid, "bbTestPlugin",
+            values[CredsNames.WebhookUrlKey] + "?wsdl",
+            (int)eventType);
             
-            await Logger.LogAsync(new { result_message = result.statusMessage, status_code = result.statusCode });
-            await creds.Logout();
-        }
-        catch (Exception e)
-        {
-            await Logger.LogAsync(e);
-            throw;
-        }
+        await Logger.LogAsync(new { result_message = result.statusMessage, status_code = result.statusCode });
+        await creds.Logout();
     }
 
     public async Task DeregisterCallback(IEnumerable<AuthenticationCredentialsProvider> creds,
         Dictionary<string, string> values,
         EventType eventType)
     {
-        try
+        await Logger.LogAsync(new { values, eventType = eventType.ToString(), message = "Unsubscribing callback" });
+        var uuid = creds.GetAuthToken();
+
+        var dataAdminClient = Clients.GetAdminClient(creds.GetInstanceUrl());
+
+        var callbacks = await dataAdminClient.getListOfRegisteredCallbacksAsync(uuid);
+        var eventCallbacks = callbacks.data.Where(c => c.eventType == (int)eventType).ToList();
+            
+        await Logger.LogAsync(new { eventCallbacks });
+
+        await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
+        var result = await orderClient.deregisterCallback_NotifyAsync(uuid, (int)eventType);
+
+        await Logger.LogAsync(new { result_message = result.statusMessage, status_code = result.statusCode });
+            
+        foreach(var callback in eventCallbacks.Where(x => x.serverAddress != values[CredsNames.WebhookUrlKey] + "?wsdl"))
         {
-            await Logger.LogAsync(new { values, eventType = eventType.ToString(), message = "Unsubscribing callback" });
-            var uuid = creds.GetAuthToken();
-
-            var dataAdminClient = Clients.GetAdminClient(creds.GetInstanceUrl());
-
-            var callbacks = await dataAdminClient.getListOfRegisteredCallbacksAsync(uuid);
-            var eventCallbacks = callbacks.data.Where(c => c.eventType == (int)eventType).ToList();
+            await orderClient.registerCallback_NotifyAsync(uuid, 
+                "bbTestPlugin",
+                callback.serverAddress, 
+                (int)eventType);
+        } 
             
-            await Logger.LogAsync(new { eventCallbacks });
-
-            await using var orderClient = Clients.GetOrderClient(creds.GetInstanceUrl());
-            var result = await orderClient.deregisterCallback_NotifyAsync(uuid, (int)eventType);
-
-            await Logger.LogAsync(new { result_message = result.statusMessage, status_code = result.statusCode });
-            
-            foreach(var callback in eventCallbacks.Where(x => x.serverAddress != values[CredsNames.WebhookUrlKey] + "?wsdl"))
-            {
-                await orderClient.registerCallback_NotifyAsync(uuid, 
-                    "bbTestPlugin",
-                    callback.serverAddress, 
-                    (int)eventType);
-            } 
-            
-            await creds.Logout();
-        }
-        catch (Exception e)
-        {
-            await Logger.LogAsync(e);
-            throw;
-        }
+        await creds.Logout();
     }
 }
