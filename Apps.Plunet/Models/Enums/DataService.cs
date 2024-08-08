290e7d88de2abbf8bd7b3a90c16c590b7af5682d
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-08-08 17:02:59 +0200

    Fixed two bugs around unsubscriptions

---
+++ Apps.Plunet/Models/Enums/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Plunet.Models.Enums
+{
+    public enum DataService
+    {
+        CustomerService = 1,
+        ItemService = 10,
+        JobService = 11,
+        OrderService = 9,
+    }
+}
