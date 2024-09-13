7561a5145650f35881e85e09eea546042379ba61
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-13 17:27:03 +0300

    Added handler

--- Apps.Plunet/DataSourceHandlers/PayableDataSourceHandler.cs
+++ Apps.Plunet/DataSourceHandlers/PayableDataSourceHandler.cs
@@ -1,7 +1,8 @@
-﻿using Apps.Plunet.Actions;
+﻿using Apps.Plunet.Constants;
 using Apps.Plunet.Invocables;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Plugins.Plunet.DataPayable30Service;
 
 namespace Apps.Plunet.DataSourceHandlers;
 
@@ -11,13 +12,49 @@ public class PayableDataSourceHandler(InvocationContext invocationContext)
     public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context,
         CancellationToken cancellationToken)
     {
-        var invoiceActions = new PayableActions(InvocationContext, null);
-        var invoices = await invoiceActions.SearchPayables(new ());
+        var response = await ExecuteWithRetry<IntegerArrayResult>(async () => await PayableClient.searchAsync(Uuid, new()));
+
+        if (response.statusMessage != ApiResponses.Ok)
+            throw new(response.statusMessage);
+
+        if (response.data is null)
+        {
+            return new();
+        }
         
-        return invoices.Items
+        return response.data
             .Where(x => context.SearchString == null ||
-                        x.ExternalInvoiceNumber.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
-            .Take(20)
-            .ToDictionary(x => x.Id, x => x.ExternalInvoiceNumber);
+                        x.Value.ToString() == context.SearchString)
+            .ToDictionary(x => x.ToString(), x => x.ToString());
+    }
+    
+    private async Task<T> ExecuteWithRetry<T>(Func<Task<Result>> func, int maxRetries = 10, int delay = 1000)
+        where T : Result
+    {
+        var attempts = 0;
+        while (true)
+        {
+            var result = await func();
+
+            if (result.statusMessage == ApiResponses.Ok)
+            {
+                return (T)result;
+            }
+
+            if (result.statusMessage.Contains("session-UUID used is invalid"))
+            {
+                if (attempts < maxRetries)
+                {
+                    await Task.Delay(delay);
+                    await RefreshAuthToken();
+                    attempts++;
+                    continue;
+                }
+
+                throw new($"No more retries left. Last error: {result.statusMessage}, Session UUID used is invalid.");
+            }
+
+            return (T)result;
+        }
     }
 }
