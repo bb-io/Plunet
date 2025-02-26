c5b17c994df4210cf9db2149283c2b1378fef9ca
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-26 17:22:56 +0200

    Update On request status changed

--- Apps.Plunet/Webhooks/WebhookLists/RequestHooks.cs
+++ Apps.Plunet/Webhooks/WebhookLists/RequestHooks.cs
@@ -52,9 +52,7 @@ public class RequestHooks(InvocationContext invocationContext) : PlunetWebhookLi
         Description = "Triggered when a request status is changed")]
     public Task<WebhookResponse<RequestResponse>> RequestChanged(WebhookRequest webhookRequest,
         [WebhookParameter][Display("New status")][StaticDataSource(typeof(RequestStatusDataHandler))] string? newStatus,
-        [WebhookParameter] GetRequestOptionalRequest optonalRequest,
-        [WebhookParameter] CustomerIdFilter customerIdFilter)
+        [WebhookParameter] GetRequestOptionalRequest optonalRequest)
         => HandleWebhook(webhookRequest, request => (newStatus == null || newStatus == request.Status) &&
-                                                    (optonalRequest.RequestId == null || optonalRequest.RequestId == request.RequestId) &&
-                                                    (customerIdFilter == null || customerIdFilter.CustomerId == request.CustomerId));
+                                                    (optonalRequest.RequestId == null || optonalRequest.RequestId == request.RequestId));
 }
