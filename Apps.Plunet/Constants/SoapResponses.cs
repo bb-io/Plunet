851319c8f552038373e3d8c40be631eaef86a72f
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2025-03-13 11:30:24 +0200

    Update ItemCallbackOk SOAP response format for consistency; Changed ItemCallbackOk response to use the standard SOAP envelope format for improved integration with API notifications.

--- Apps.Plunet/Constants/SoapResponses.cs
+++ Apps.Plunet/Constants/SoapResponses.cs
@@ -4,5 +4,5 @@ public static class SoapResponses
 {
     public const string CustomerAndResourceOk = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:api=\"http://API.Integration/\"><soap:Header/><soap:Body><api:receiveNotifyCallbackResponse/></soap:Body></soap:Envelope>";
     public const string OtherOk = "<S:Envelope xmlns:S=\"http://www.w3.org/2003/05/soap-envelope\"><S:Body><ns2:ReceiveNotifyCallbackResponse xmlns:ns2=\"http://API.Integration/\"/></S:Body></S:Envelope>";
-    public const string ItemCallbackOk = @"<S:Envelope xmlns:S=""http://www.w3.org/2003/05/soap-envelope""><S:Body><ns2:receiveNotifyCallback xmlns:ns2=""http://API.Integration/""></ns2:receiveNotifyCallback></S:Body></S:Envelope>";
+    public const string ItemCallbackOk = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:api=\"http://API.Integration/\"><soap:Header/><soap:Body><api:receiveNotifyCallbackResponse/></soap:Body></soap:Envelope>";
 }
