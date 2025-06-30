e277820bb932a0b855d06fb1ad8560314fd087fc
RiabushenkoA <ariabushenko@blackbird.io>
2025-06-30 19:55:43 +0300

    Added input for tax type in actions for pricelines

--- Apps.Plunet/DataSourceHandlers/EnumHandlers/TaxTypeDataHandler.cs
+++ Apps.Plunet/DataSourceHandlers/EnumHandlers/TaxTypeDataHandler.cs
@@ -13,24 +13,24 @@ namespace Apps.Plunet.DataSourceHandlers.EnumHandlers
         {
             return new()
             {
-                { "INFO", "3" },
-                { "INFO_SUM", "6" },
-                { "PRICE_BLOCK", "12" },
-                { "SUM", "4" },
-                { "TAX_1", "0" },
-                { "TAX_1_2", "5" },
-                { "TAX_1_2_3", "8" },
-                { "TAX_1_2_3_4", "14" },
-               //{ "TAX_1_2_3_4_5", "14" },   duplicateв value
-                { "TAX_1_3", "10" },
-                { "TAX_1_4", "17" },
-                { "TAX_2", "1" },
-                { "TAX_2_3", "11" },
-                { "TAX_2_4_5", "16" },
-                { "TAX_3", "7" },
-                { "TAX_4", "9" },
-                { "TAX_5", "13" },
-                { "WITHOUT_TAX", "2" }
+                 { "3", "INFO" },
+                { "6", "INFO_SUM" },
+                { "12", "PRICE_BLOCK" },
+                { "4", "SUM" },
+                { "0", "TAX_1" },
+                { "5", "TAX_1_2" },
+                { "8", "TAX_1_2_3" },
+                { "14", "TAX_1_2_3_4" },
+                { "10", "TAX_1_3" },
+                { "17", "TAX_1_4" },
+                { "1", "TAX_2" },
+                { "11", "TAX_2_3" },
+                { "16", "TAX_2_4_5" },
+                { "7", "TAX_3" },
+                { "9", "TAX_4" },
+                { "13", "TAX_5" },
+                { "2", "WITHOUT_TAX" }              
+               //{ "14", "TAX_1_2_3_4_5" },   duplicate value             
             };
         }
     }
