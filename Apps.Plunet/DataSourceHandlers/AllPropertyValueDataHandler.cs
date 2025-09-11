54c33204ea5e993f22b301b4173d18ffd5c4912b
RiabushenkoA <ariabushenko@blackbird.io>
2025-09-11 14:55:43 +0300

    Added filter to action 'Search resources'

---
+++ Apps.Plunet/DataSourceHandlers/AllPropertyValueDataHandler.cs
@@ -0,0 +1,48 @@
+﻿using Apps.Plunet.Invocables;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Plugins.Plunet.DataCustomFields30;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.Plunet.DataSourceHandlers
+{
+    public class AllPropertyValueDataHandler : PlunetInvocable, IAsyncDataSourceHandler
+    {
+        public AllPropertyValueDataHandler(InvocationContext invocationContext) : base(invocationContext)
+        {
+        }
+
+        public async Task<Dictionary<string, string>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
+        {
+            var client = new DataCustomFields30Client();
+            var properties = await ExecuteWithRetryAcceptNull(() => client.getAvailablePropertiesAsync(Uuid, 2), cancellationToken);
+
+            var result = new Dictionary<string, string>();
+
+            if (properties == null || properties.Length == 0)
+                return result;
+
+            foreach (var prop in properties)
+            {
+                if (prop.avaliablePropertyValueIDList != null)
+                {
+                    foreach (var valueId in prop.avaliablePropertyValueIDList.Where(id => id.HasValue))
+                    {
+                        var id = valueId.Value.ToString();
+                        var displayValue = $"{prop.propertyNameEnglish}: {id}";
+                        if (context.SearchString == null || displayValue.Contains(context.SearchString, StringComparison.OrdinalIgnoreCase))
+                        {
+                            result[id] = displayValue;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Apps.Plunet/DataSourceHandlers/EnumHandlers/PropertyTypeDataHandler.cs
@@ -0,0 +1,16 @@
+﻿using Blackbird.Applications.Sdk.Common.Dictionaries;
+
+namespace Apps.Plunet.DataSourceHandlers.EnumHandlers
+{
+    public class PropertyTypeDataHandler : IStaticDataSourceHandler
+    {
+        public Dictionary<string, string> GetData()
+        {
+            return new Dictionary<string, string>
+            {
+                { "1", "Single select" },
+                { "2", "Multi select" }
+            };
+        }
+    }
+}
