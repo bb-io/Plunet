1186e4eee047ac2ecdd77c9c022c6346865536cc
Alex Terehov <terales@users.noreply.github.com>
2025-11-25 10:41:37 -0330

    Extract custom property into dedicated test class

---
+++ Tests.Plunet/CustomPropertyActionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Apps.Plunet.Actions;
+using Apps.Plunet.Models.CustomProperties;
+using Newtonsoft.Json;
+using Tests.Plunet.Base;
+
+namespace Tests.Plunet;
+
+[TestClass]
+internal class CustomPropertyActionsTests : TestBase
+{
+    [TestMethod]
+    public async Task GetTextModule_IsSuccess()
+    {
+        // Arrange
+        var action = new CustomPropertyActions(InvocationContext);
+        var request = new TextModuleRequest
+        {
+            Flag = "[XTM-User-ID]",
+            UsageArea = "2",
+            MainId = "67"
+        };
+
+        // Act
+        var response = await action.GetTextmodule(request);
+
+        // Assert
+        Assert.IsNotNull(response);
+        Console.WriteLine(JsonConvert.SerializeObject(response, Formatting.Indented));
+    }
+}
